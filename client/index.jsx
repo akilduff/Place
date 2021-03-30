5db85f2495883f54d02669ea3d96c89564fee46b
Andrew Kilduff <a.l.kilduff@gmail.com>
2021-03-29 22:53:30 -0700

    Update to prep for implementation of react router

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Place from './place.jsx';
+import axios from 'axios';
+import styled from 'styled-components';
 
-const currentView = window.location.pathname;
+import Header from './components/header.jsx'
+import Footer from './components/footer.jsx'
+import Landing from './components/landing.jsx'
+import About from './components/about.jsx'
 
-ReactDOM.render(<Place view={currentView}/>, document.getElementById('Place'));
+class Place extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      view: 'home'
+    };
+
+    this.handleView = this.handleView.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('Current View: ', this.props.view)
+  }
+
+  handleView(e) {
+    let wantedView = e.target.id;
+    this.setState({
+      view: wantedView
+    })
+    e.preventDefault();
+  }
+
+  render() {
+    if (this.state.view === 'home') {
+      return (
+        <>
+          <Header/>
+          <Landing/>
+          <Footer handleView={this.handleView}/>
+        </>
+      )
+    } else if (this.state.view === 'about') {
+      return (
+        <>
+          <About/>
+          <Footer handleView={this.handleView}/>
+        </>
+      )
+    }
+  }
+}
+
+ReactDOM.render(<Place/>, document.getElementById('Place'));
